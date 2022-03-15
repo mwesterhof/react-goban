c45722d6b107a888c767efeda81dac0bc33e0acb
Marco Westerhof <m.westerhof@lukkien.com>
2022-03-15 22:25:48 +0100

    dynamic size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Goban from './components/Goban';
 
+
 function App() {
-  return (
-    <div className="App">
-        <Goban size={19} />
-    </div>
-  );
-}
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const size = queryParams.get('size') || 19;
+
+    return (
+        <div className="App">
+            <Goban size={size} />
+        </div>
+    );
+};
 
 export default App;
